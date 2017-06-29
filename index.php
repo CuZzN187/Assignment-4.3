bc27c399359df900e423eb13ab338f72f5767674
CuZzN187 <boogens04@aol.com>
2017-06-28 23:02:07 -0600

    Assignment 4.3

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 include "mySQL.php";
 
-$asin = false;
+//$asin = false;
 
 echo "<!DOCTYPE html>
 	<html><head>
@@ -11,75 +11,75 @@ echo "<!DOCTYPE html>
 	<p>Assignment 4.2</p><br>
 	</body></html>";
 
-echo "<H2>Initial Table with 1 Movie</H2><br>"; //DODGEBALL
+/////////////////////////////////////////////////////////////////////////////////
+//List Testing
+
+echo "<H2>Initial Table with No Movies</H2><br>"; 
 
 $table = "dvdtitles";
 
 $stmt = fListFromDatabase($table);
 
-////////////////////////////////////////////////////////////////////////////////
-
-echo "<br><H2>Add 3 Movies to Database</H2><br>";
-//Deadpool, MI4, Fast 8
-$movieASINs = array("B01BLS9EVA", "B00VTDQJUM", "B06Y3KS6S9"); 
-$movieNames = array("Deadpool","MI4","Fast 8");
-$moviePrice = array(10.77, 7.39, 19.96);
-for ($x = 0; $x < 3; $x++)
-{
-	$stmt = fInsertToDatabase($movieASINs[$x], $movieNames[$x], $moviePrice[$x]);
-}
-
-$stmt = fListFromDatabase($table);
-
-/////////////////////////////////////////////////////////////////////////////////
-
-echo "<br><H2>Delete 2 Movies from Database</H2><br>";
-
-for ($d = 1; $d < 3; $d++)
-{
-	$stmt = fDeleteFromDatabase($table, $movieASINs[$d]);
-}
-
-$stmt = fListFromDatabase($table);
-//Reset Movie Table
-$stmt = fDeleteFromDatabase($table, $movieASINs[0]);
-
-/////////////////////////////////////////////////////////////////////////////////
-
-echo "<H2>Initial Table with 2 Actors</H2><br>"; 
+echo "<H2>Initial Table with No Actors</H2><br>"; 
 
 $table = "dvdActors";
 
-$stmt = fListFromDatabase($table);// Ben Stiller and Vince Vaughn
+$stmt = fListFromDatabase($table);
+
+echo "<H2>Initial Table with No Joins</H2><br>"; 
+
+$table = "dvdJoin";
+
+$stmt = fListFromDatabase($table);
 
 ////////////////////////////////////////////////////////////////////////////////
+//Insert Testing
 
-echo "<br><H2>Add 6 Actors to Database</H2><br>";
+//Insert Movies
+$table = "dvdtitles";
+echo "<br><H2>Add 4 Movies to Database</H2><br>";
+//Dodgeball, Deadpool, MI4, Fast 8
+$movieASINs = array("B00193F304", "B00VTDQJUM","B01BLS9EVA", "B06Y3KS6S9"); 
+$movieNames = array("Dodgeball", "MI4", "Deadpool", "Fast 8");
+$moviePrice = array(5.00, 10.77, 7.50, 14.99);
+for ($x = 0; $x < 4; $x++)
+{
+	$stmt = fInsertToDatabase($movieASINs[$x], $movieNames[$x], $moviePrice[$x]);//add to dvdtitles
+}
 
-$actorFirst = array("Tom", "Jeremy", "Ryan", "Morena", "Vin", "Dwayne"); 
-$actorLast = array("Cruise", "Renner", "Reynolds", "Baccarin", "Diesel", "Johnson");
-for ($x = 0; $x < 6; $x++)
+$stmt = fListFromDatabase($table);
+//Insert Actors
+$table = "dvdActors";
+echo "<br><H2>Add 8 Actors to Database</H2><br>";
+$actorFirst = array("Ben", "Vince", "Tom", "Jeremy", "Ryan", "Morena", "Vin", "Dwayne"); 
+$actorLast = array("Stiller" ,"Vaughn", "Cruise", "Renner", "Reynolds", "Baccarin", "Diesel", "Johnson");
+$asin = false;
+for ($x = 0; $x < 8; $x++)
 {
 	$stmt = fInsertToDatabase($asin, $actorFirst[$x], $actorLast[$x]);
 }
 
 $stmt = fListFromDatabase($table);
-
-/////////////////////////////////////////////////////////////////////////////////
-
-echo "<br><H2>Delete 3 Actors from Database</H2><br>";
-
-for ($dd = 3; $dd < 6; $dd++)
+//Insert into dvdJoin table
+$joinASIN = array("B00193F304", "B00193F304", "B00VTDQJUM", "B00VTDQJUM", "B01BLS9EVA", "B01BLS9EVA", "B06Y3KS6S9", "B06Y3KS6S9");
+$table = "dvdJoin";
+$aee = xtra();
+//insert into database for testing
+for($x = 0; $x<8; $x++)
 {
-	$stmt = fDeleteFromDatabase($table, $actorLast[$dd]);
+	$stmt = fInsertToDatabase($table, $joinASIN[$x], $aee[$x]);
 }
-
 $stmt = fListFromDatabase($table);
-//Reset Actors Table
-for ($ddd = 0; $ddd < 3; $ddd++)
-{
-	$stmt = fDeleteFromDatabase($table, $actorLast[$ddd]);
-}
+
+//////////////////////////////////////////////////////////////////////////////////////
+//Delete Testing
+
+//Join Table after above deletes
+echo ("<H2>Delete MI4 from Relational Table and ALL OTHER RELATING TABLES</H2>");
+$table1 = "dvdJoin";
+$stmt = fDeleteFromDatabase($table1, "B00VTDQJUM"); //delete MI4
+$stmt = fListFromDatabase($table1);
+
 
 ?>
 
--- mySQL.php
+++ mySQL.php
@@ -4,24 +4,47 @@ include "dbConnect.php";
 
 function fInsertToDatabase($asin, $title, $price) {
 	$myDB = fConnectToDatabase();
-	if(!$asin){
+	if(!$asin){//$asin passed in as a false boolean to default to dvdActors table
 		$sql = "INSERT INTO dvdActors (fname, lname) VALUES ('$title', '$price')";
+		$stmt = $myDB->prepare($sql);
+		$stmt->execute();
+	}elseif($asin == "dvdJoin"){
+		$sql = "INSERT INTO dvdJoin (asin, actorID) VALUES ('$title', $price)";
+		$stmt = $myDB->prepare($sql);
+		$stmt->execute();
 	}else{
 		$sql = "INSERT INTO dvdtitles (asin, title, price) VALUES ('$asin', '$title', $price)";
+		$stmt = $myDB->prepare($sql);
+		$stmt->execute();
 	}
-	$stmt = $myDB->prepare($sql);
-	$stmt->execute();
+	
 	//TODO fill in the rest of this function
 }
 
 function fDeleteFromDatabase($table, $asin) {
 	$myDB = fConnectToDatabase();
 	if($table == "dvdtitles"){
-		$sql = "DELETE FROM $table WHERE asin='$asin'";
-	}
+		$sql = "DELETE FROM $table WHERE asin='$asin'"; 
+	} 	
 	if($table == "dvdActors"){
-
-		$sql = "DELETE FROM $table WHERE lname='$asin'";
+		$sql = "DELETE FROM $table WHERE actorID='$asin'";
+	}
+	if($table == "dvdJoin"){
+		$stmt = $myDB->prepare("SELECT tt.asin, a.actorID FROM CS3620.dvdtitles tt INNER JOIN CS3620.dvdJoin i ON tt.asin = i.asin INNER JOIN 
+		CS3620.dvdActors a ON a.actorID = i.actorID WHERE tt.asin = '$asin'");
+		$stmt->execute();
+		while($row = $stmt->fetch()){
+			$sql = "DELETE FROM $table WHERE asin = '$row[0]'";
+			$sql1 = "DELETE FROM dvdtitles WHERE asin = '$row[0]'";
+			$sql2 = "DELETE FROM dvdActors WHERE actorID = '$row[1]'";
+			print_r($row[0]." ");
+			echo ("success!");
+			echo "<br>";
+		}
+		$stmt = $myDB->prepare($sql1);
+		$stmt->execute();
+		$stmt = $myDB->prepare($sql2);
+		$stmt->execute();
 	}
 	$stmt = $myDB->prepare($sql);
 	$stmt->execute();
@@ -48,5 +71,40 @@ function fListFromDatabase($table) {
 		}
 		return $stmt;
 	}
+	if($table == "dvdJoin"){
+		$stmt = $myDB->prepare("SELECT asin, actorID FROM dvdJoin");
+		$stmt->execute();
+		while($row = $stmt->fetch()){
+			print_r($row[0]." ".$row[1]);
+			echo "<br>";
+		}
+		return $stmt;
+	}
 	//TODO fill in the rest of this function
+}
+
+function fJoinFromDatabase($asin){
+	$myDB = fConnectToDatabase();
+	$stmt = $myDB->prepare("SELECT tt.asin, a.actorID FROM CS3620.dvdtitles tt INNER JOIN CS3620.dvdJoin i ON tt.asin = i.asin INNER JOIN 
+		CS3620.dvdActors a ON a.actorID = i.actorID");
+	$stmt->execute();
+	while($row = $stmt->fetch()){
+			print_r($row[0]." ".$row[1]." ");
+			echo "<br>";
+			//$url = 'http://images.amazon.com/images/P/'.$row[0].'.01.MZZZZZZZ.jpg';
+			//echo('<!DOCTYPE html><html><head><title>title</title></head><body><td><img src="'.$url.'"</td><br></body></html>');
+		}
+		return $stmt;
+}
+
+function xtra(){
+	$myDB = fConnectToDatabase();
+	$stmt = $myDB->prepare("SELECT actorID FROM dvdActors");
+	$stmt->execute();
+	$test = array();
+	echo ("<h2>Add 8 Records to Relational Database</h2>");
+	while($row = $stmt->fetch()){
+			array_push($test, $row[0]);
+		}
+	return $test;
 }
